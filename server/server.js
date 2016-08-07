c1207fbd5d2715a90b433a8e1dd54c44bc4e2b3a
Atlante <atlanteavila@gmail.com>
2016-08-07 10:19:42 -0700

    (Backend) added the mogoose connect to server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,15 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+// connect our app to mongodb
+mongoose.connect('mongodb://localhost/cbb');
+
+
+// require the Owner model for our db
+var Owner = require('./../app/scripts/models/owner');
+var Dog = require('./../app/scripts/models/dog');
+
+
 // adding the ability to serve out static html files within express
 app.use(express.static(__dirname + '/public'));
 
